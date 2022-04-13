ff6f7f9881254e143a7bcf6fbff1e57dcc44b582
Eli-Asher <asher3@wisc.edu>
2022-04-13 18:28:00 -0500

    Working New code

--- test.py
+++ test.py
@@ -1,4 +1,158 @@
-print('Here')
-#poop
-print("Over here")
-print("last one")
+import numpy as np
+import pandas as pd
+import os
+from time import time
+import errno
+import shutil
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
+from tensorflow.python.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.python.keras.models import Sequential
+from tensorflow.python.keras.layers import Dense,Flatten,GlobalAveragePooling2D,Conv2D,MaxPooling2D,Activation,Dropout
+
+import sklearn
+from sklearn.datasets import fetch_lfw_people      ######
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report, log_loss, accuracy_score
+from sklearn.model_selection import train_test_split
+#from sklearn.decomposition import PCA
+#from sklearn.svm import SVC
+#from sklearn.model_selection import GridSearchCV
+def copy(src, dest):
+    try:
+        shutil.copytree(src, dest)
+    except OSError as e:
+        if e.errno == errno.ENOTDIR:
+            shutil.copy(src, dest)
+        else:
+            print('Directory not copied. Error: %s' % e)
+            
+src = '../input/'
+dest = '../LFW/lfw_home'
+copy(src,dest) 
+#### copy work is essential
+###### read_only_
+path = '../LWF' 
+lfw_dataset=sklearn.datasets.fetch_lfw_people(data_home=path,min_faces_per_person=100,download_if_missing=True)
+print(lfw_dataset.images.shape)
+print(lfw_dataset.data.shape)
+print(lfw_dataset.target_names.shape)
+print(lfw_dataset.target.shape)
+print(lfw_dataset.images.shape)
+print(lfw_dataset.data.shape)
+print(lfw_dataset.target_names.shape)
+print(lfw_dataset.target.shape)
+print(lfw_dataset.target)
+Name = lfw_dataset.target_names
+
+print(Name)
+print(len(Name))
+N=[]
+for i in range(len(Name)):
+    N+=[i]
+    
+mapping=dict(zip(Name,N)) 
+reverse_mapping=dict(zip(N,Name)) 
+
+def mapper(value):
+    return reverse_mapping[value]
+X0=lfw_dataset.images
+y=lfw_dataset.target
+print(X0.shape)
+X=X0.reshape(-1,62,47,1)
+print(X.shape)
+dataset=[]
+testset=[]
+t=0
+for Xi,yi in zip(X,y):
+    img=Xi/255.0
+    if t<=200:
+        dataset.append([img,yi])
+    else:   
+        testset.append([img,yi])
+    t+=1
+data,labels0=zip(*dataset)
+test,tlabels0=zip(*testset)
+labels1=to_categorical(labels0)
+data=np.array(data)
+labels=np.array(labels1)
+tlabels1=to_categorical(tlabels0)
+test=np.array(test)
+tlabels=np.array(tlabels1)
+trainx,testx,trainy,testy=train_test_split(data,labels,test_size=0.2,random_state=44)
+print(trainx.shape)
+print(testx.shape)
+print(trainy.shape)
+print(testy.shape)
+datagen = ImageDataGenerator(horizontal_flip=True,vertical_flip=True,rotation_range=20,zoom_range=0.2,
+                    width_shift_range=0.2,height_shift_range=0.2,shear_range=0.1,fill_mode="nearest")
+model = Sequential()
+
+model.add(Conv2D(32,(3,3), input_shape=(62,47,1), activation='relu'))
+model.add(MaxPooling2D(2,2))
+model.add(Conv2D(32,(3,3), activation='relu'))
+model.add(MaxPooling2D(2,2))
+model.add(Flatten())
+model.add(Dense(units=512, activation='relu'))
+model.add(Dense(units=128, activation='relu'))
+model.add(Dense(units=5, activation='softmax'))
+model.summary()
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+his=model.fit(datagen.flow(trainx,trainy,batch_size=32),validation_data=(testx,testy),epochs=1000)
+y_pred=model.predict(testx)
+pred=np.argmax(y_pred,axis=1)
+ground=np.argmax(testy,axis=1)
+print(classification_report(ground,pred))
+get_acc = his.history['accuracy']
+value_acc = his.history['val_accuracy']
+get_loss = his.history['loss']
+validation_loss = his.history['val_loss']
+
+epochs = range(len(get_acc))
+plt.plot(epochs, get_acc, 'r', label='Accuracy of Training data')
+plt.plot(epochs, value_acc, 'b', label='Accuracy of Validation data')
+plt.title('Training vs validation accuracy')
+plt.legend(loc=0)
+plt.figure()
+plt.show()
+epochs = range(len(get_loss))
+plt.plot(epochs, get_loss, 'r', label='Loss of Training data')
+plt.plot(epochs, validation_loss, 'b', label='Loss of Validation data')
+plt.title('Training vs validation loss')
+plt.legend(loc=0)
+plt.figure()
+plt.show()
+def plot_gallery(images, titles, h, w, n_row=1, n_col=4):
+    """Helper function to plot a gallery of portraits"""
+    plt.figure(figsize=(1.8 * n_col, 2.4 * n_row))
+    plt.subplots_adjust(bottom=0, left=.01, right=.99, top=.90, hspace=.35)
+    for i in range(n_row * n_col):
+        plt.subplot(n_row, n_col, i+1)
+        plt.imshow(images[i].reshape((h, w)), cmap=plt.cm.gray)
+        plt.title(titles[i], size=12)
+        plt.xticks(())
+        plt.yticks(())
+test1=test[0:4].reshape(4,62*47)
+plot_gallery(test1,'1234',62,47)
+print(test[0:4].shape)
+print(test1.shape)
+prediction_image=test[0:4]
+prediction=model.predict(prediction_image)
+print(prediction)
+for i in range(4):
+    value=np.argmax(prediction[i])
+    name=mapper(value)
+    print("Prediction {} is {}.".format(i+1,name))
+print(test.shape)
+prediction2=model.predict(test)
+print(prediction2.shape)
+
+PRED=[]
+for item in prediction2:
+    value2=np.argmax(item)      
+    PRED+=[value2]
+ANS=tlabels0
+accuracy=accuracy_score(ANS,PRED)
+print(accuracy)
